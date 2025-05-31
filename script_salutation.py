c29f2169b74d4ebdc4cc3f1afab96349278446bd
Nakarot <nadim@kakarot.org>
2025-05-31 15:33:11 +0200

    feat: Ajout d'une interaction utilisateur dans le script de salutation

--- script_salutation.py
+++ script_salutation.py
@@ -4,4 +4,14 @@ from utils.helpers import greet
 print("Bonjour le monde Git !")
 print("Ceci est mon premier script Python géré par Git.")
 print("J'ajoute une nouvelle ligne après avoir appris à pousser sur GitHub.")
+print(greet("débutant en Git"))
+
+# Nouvelle fonctionnalité : Interaction utilisateur
+nom_utilisateur = input("Quel est votre nom ? ")
+print(f"Heureux de vous rencontrer, {nom_utilisateur} !")# script_salutation.py
+from utils.helpers import greet
+
+print("Bonjour le monde Git !")
+print("Ceci est mon premier script Python géré par Git.")
+print("J'ajoute une nouvelle ligne après avoir appris à pousser sur GitHub.")
 print(greet("débutant en Git")) # Nouvelle ligne
